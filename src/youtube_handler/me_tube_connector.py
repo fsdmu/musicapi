80eeaf4a4f73f58b8c5e96228d31cf57c08f80ae
Felix <felix.schulte-derne@hm.edu>
2025-12-24 21:58:12 +0100

    refactor download handler

--- src/youtube_handler/me_tube_connector.py
+++ src/youtube_handler/me_tube_connector.py
@@ -11,7 +11,7 @@ import logging
 import src.logging_config  # noqa: F401
 
 from src.database_connector import DatabaseConnector
-from src.youtube_album_fetcher import YoutubeAlbumFetcher
+from src.youtube_handler.youtube_album_fetcher import YoutubeAlbumFetcher
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,22 +20,31 @@ logger.setLevel(logging.INFO)
 class MeTubeConnector:
     """Connector for MeTube API."""
 
-    def __init__(self, base_url: Optional[str]) -> None:
+    def __init__(self, base_url: Optional[str] = None) -> None:
         """Initialize MeTubeConnector.
 
         Args:
             base_url: Base URL for MeTube API. If None, will use the
                 ME_TUBE_API_URL environment variable.
 
+        Raises:
+            ValueError: If base_url is not provided and ME_TUBE_API_URL
+                environment variable is not set.
+
         """
         self.base_url = base_url or os.environ.get("ME_TUBE_API_URL")
+        if self.base_url is None:
+            raise ValueError(
+                "Base URL for MeTube API must be provided either as an argument "
+                "or via the ME_TUBE_API_URL environment variable."
+            )
         self.db_connector = DatabaseConnector()
 
     def queue_download(
         self,
         url: str | List[str],
         quality: str = "Best",
-        format: str = "mp3",
+        download_format: str = "mp3",
         add_without_download: bool = False,
     ) -> Optional[List[requests.Response]]:
         """Queue a download for the given URL(s).
@@ -43,7 +52,7 @@ class MeTubeConnector:
         Args:
             url: A single YouTube URL or a list of URLs to queue for download.
             quality: Desired quality of the download. Default is "Best".
-            format: Desired format of the download. Default is "mp3".
+            download_format: Desired download_format of the download. Default is "mp3".
             add_without_download: If True, will add the URL to the
                 database without queuing a download.
 
@@ -55,20 +64,29 @@ class MeTubeConnector:
         if type(url) is str:
             url = [url]
         responses = []
-        for url in url:
-            if "playlist" in url:
-                album_result = self.db_connector.get_album(url)
+        for single_url in url:
+
+            is_song = "watch" in single_url
+            is_playlist = "playlist" in single_url
+
+            if is_playlist:
+                album_result = self.db_connector.get_album(single_url)
                 if album_result is not None:
-                    logger.info(f"Album {url} already in database, skipping.")
+                    logger.info(f"Album {single_url} already in database, skipping.")
                     continue
-            elif "watch" in url:
-                song_result = self.db_connector.get_song(url)
+
+            elif is_song:
+                song_result = self.db_connector.get_song(single_url)
                 if song_result is not None:
-                    logger.info(f"Song {url} already in database, skipping.")
+                    logger.info(f"Song {single_url} already in database, skipping.")
                     continue
 
             if not add_without_download:
-                data = {"url": url, "quality": quality, "format": format}
+                data = {
+                    "url": single_url,
+                    "quality": quality,
+                    "format": download_format,
+                }
                 req = requests.post(
                     f"{self.base_url}/add",
                     data=json.dumps(data),
@@ -80,14 +98,17 @@ class MeTubeConnector:
                     logger.info(f"Response: {req.text}")
                     return None
 
-                logger.info(f"Successfully queued download for URL: {url}")
-            if "playlist" in url:
-                album_id = url.split("list=")[1]
-                self.db_connector.add_album(url)
+                logger.info(f"Successfully queued download for URL: {single_url}")
+
+            if is_playlist:
+                album_id = single_url.split("list=")[1].split("&")[0]
+                self.db_connector.add_album(single_url)
                 song_urls = YoutubeAlbumFetcher.get_album_songs(album_id)
                 for song_url in song_urls:
                     self.db_connector.add_song(song_url)
-            elif "watch" in url:
-                self.db_connector.add_song(url)
-
+            elif is_song:
+                self.db_connector.add_song(single_url)
+            else:
+                raise ValueError(f"Unsupported URL format: {single_url}")
+        logger.info("Responses: %s", responses)
         return responses
---
+++ src/youtube_handler/youtube_download_handler.py
@@ -0,0 +1,124 @@
+"""Handler for downloading music from YouTube."""
+
+import logging
+from typing import Optional
+
+import src.logging_config  # noqa: F401
+
+from src.database_connector import DatabaseConnector
+from src.download_handler_base import DownloadHandlerBase
+from src.youtube_handler.me_tube_connector import MeTubeConnector
+from src.youtube_handler.youtube_album_fetcher import YoutubeAlbumFetcher
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class YoutubeDownloadHandler(DownloadHandlerBase):
+    """Handler for downloading music from YouTube."""
+
+    def __init__(self, db_connector: DatabaseConnector) -> None:
+        """Initialize YoutubeDownloadHandler.
+
+        Args:
+            db_connector: An instance of DatabaseConnector for database operations.
+
+        """
+        self.mt_connector = MeTubeConnector()
+        self.db_connector = db_connector
+
+    def download(
+        self,
+        url: str,
+        auto_download: bool = False,
+        download_format: str = "mp3",
+        quality: str = "Best",
+        **kwargs,
+    ) -> None:
+        """Download music from a YouTube URL.
+
+        Args:
+            url: The YouTube URL to download from.
+            auto_download: Whether to mark an artist for auto-download.
+                This is only applicable if the URL is a channel URL. Default is False.
+            download_format: The desired download_format for the download.
+                Default is "mp3".
+            quality: The desired quality for the download. Default is "Best".
+            **kwargs: Additional keyword arguments.
+
+        Raises:
+            ValueError: If the URL format is unsupported.
+
+        """
+        add_without_download = kwargs.get("add_without_download", False)
+
+        if "channel" in url:
+            self._handle_channel_url(url, auto_download)
+        elif "playlist" in url or "watch?v=" in url:
+            self.mt_connector.queue_download(
+                url,
+                quality=quality,
+                download_format=download_format,
+                add_without_download=add_without_download,
+            )
+        else:
+            error = f"Unsupported YouTube URL format: {url}"
+            logger.error(error)
+            raise ValueError(error)
+
+    def _handle_channel_url(self, channel_url: str, auto_download: bool) -> None:
+        """Handle adding a YouTube channel URL to the database.
+
+        Args:
+            channel_url: The YouTube channel URL.
+            auto_download: Whether to mark the artist for auto-download.
+
+        """
+        album_urls = YoutubeAlbumFetcher.get_album_ids(channel_url)
+        self.db_connector.add_artist(channel_url)
+        if auto_download:
+            self.db_connector.add_auto_download_artist(channel_url)
+        for album_url in album_urls:
+            try:
+                self.mt_connector.queue_download(
+                    album_url,
+                    quality="Best",
+                    download_format="mp3",
+                    add_without_download=True,
+                )
+
+            except Exception as e:
+                raise RuntimeError(
+                    f"Error queuing download for album {album_url}: {e}"
+                ) from e
+
+            database_album_id = self.db_connector.add_album(album_url)
+            logger.info(
+                f"Added album {album_url} with ID {database_album_id} "
+                f"for artist {channel_url}"
+            )
+
+            songs = YoutubeAlbumFetcher.get_album_songs(album_url.split("list=")[1])
+            for song_url in songs:
+                database_song_id = self.db_connector.add_song(song_url)
+                logger.info(
+                    f"Added song {song_url} with ID {database_song_id} "
+                    f"for album {album_url}"
+                )
+
+    def get_warning(self, url: str) -> Optional[str]:
+        """Get a warning message for the given URL, if applicable.
+
+        Args:
+            url: The YouTube URL to check.
+
+        Returns:
+            A warning message if applicable, otherwise None.
+
+        """
+        if "youtube.com" in url and "music.youtube.com" not in url:
+            return (
+                "Using youtube.com links instead of music.youtube.com links "
+                "is discouraged."
+            )
+        return None
